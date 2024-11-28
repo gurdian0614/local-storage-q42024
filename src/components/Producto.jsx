19e2720ff9509b2994c848987cf83d641304fd26
gurdian0614 <ivan.gurdian@outlook.com>
2024-11-27 20:36:51 -0600

    configure interface

--- src/components/Producto.jsx
+++ src/components/Producto.jsx
@@ -3,7 +3,20 @@ import { useEffect } from "react"
 import useProducto from '../hooks/useProducto'
 
 const Producto  = () => {
-    const {getProductos} = useProducto()
+    const {
+      getProductos, 
+      openModal,
+      validar,
+      deleteProducto,
+      productos,
+      titleModal,
+      nombre,
+        setNombre,
+        descripcion,
+        setDescripcion,
+        precio,
+        setPrecio,
+    } = useProducto()
     
     useEffect(() => {
         getProductos()
@@ -14,7 +27,7 @@ const Producto  = () => {
         <div className="row mt-3">
           <div className="col-md-4 offset-md-4">
             <div className="d-grid mx-auto">
-              <button
+              <button onClick={() => openModal(1)}
                 className="btn btn-primary"
                 data-bs-toggle="modal"
                 data-bs-target="#modalProductos"
@@ -38,7 +51,26 @@ const Producto  = () => {
                     <th>Acciones</th>
                   </tr>
                 </thead>
-                <tbody className="table-group-divider"></tbody>
+                <tbody className="table-group-divider">
+                  {
+                    productos.map((producto, i) =>(
+                      <tr key={producto.id}>
+                        <td>{i + 1}</td>
+                        <td>{producto.nombre}</td>
+                        <td>{producto.descripcion}</td>
+                        <td>{producto.precio.toLocaleString('es-HN', { style: 'currency', currency: 'HNL'})}</td>
+                        <td>
+                          <button onClick={() => openModal(2, producto.id, producto.nombre, producto.descripcion, producto.precio)} className='btn btn-warning' data-bs-toggle='modal' data-bs-target = '#modalProductos'>
+                            <i className='fa-solid fa-edit' />
+                          </button>
+                          <button onClick={() => deleteProducto(producto.id)} className='btn btn-danger'>
+                            <i className='fa-solid fa-trash' />
+                          </button>
+                        </td>
+                      </tr>
+                    ))
+                  }
+                </tbody>
               </table>
             </div>
           </div>
@@ -48,7 +80,7 @@ const Producto  = () => {
           <div className="modal-dialog">
             <div className="modal-content">
               <div className="modal-header">
-                <label className="h5">Agregar producto</label>
+                <label className="h5">{titleModal}</label>
                 <button
                   className="btn-close"
                   data-bs-dismiss="modal"
@@ -60,24 +92,24 @@ const Producto  = () => {
 
                 <div className="input-group mb-3">
                     <span className="input-group-text"><i className="fa-solid fa-gift" /></span>
-                    <input type="text" id='nombre' className="form-control" placeholder="Nombre" />
+                    <input type="text" id='nombre' className="form-control" placeholder="Nombre" value={nombre} onChange={(e) => setNombre(e.target.value)} />
                 </div>
 
                 <div className="input-group mb-3">
                     <span className="input-group-text"><i className="fa-solid fa-comment" /></span>
-                    <input type="text" id='descripcion' className="form-control" placeholder="Descripción" />
+                    <input type="text" id='descripcion' className="form-control" placeholder="Descripción" value={descripcion} onChange={(e) => setDescripcion(e.target.value)} />
                 </div>
 
                 <div className="input-group mb-3">
                     <span className="input-group-text"><i className="fa-solid fa-dollar-sign" /></span>
-                    <input type="text" id='precio' className="form-control" placeholder="Precio" />
+                    <input type="text" id='precio' className="form-control" placeholder="Precio" value={precio} onChange={(e) => setPrecio(e.target.value)} />
                 </div>
               </div>
               <div className="modal-footer">
-                <button className="btn btn-success">
+                <button className="btn btn-success" onClick={() => validar()}>
                     <i className="fa-solid fa-floppy-disk" /> Guardar
                 </button>
-                <button className="btn btn-danger" data-bs-dismiss="modal">Cerrar</button> 
+                <button id="btnCerrarModal" className="btn btn-danger" data-bs-dismiss="modal">Cerrar</button> 
               </div>
             </div>
           </div>
